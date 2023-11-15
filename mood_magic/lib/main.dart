91d6a2de6121fa40f97799a6cf66d5fe12fe3616
tlee85 <114027715+tlee85@users.noreply.github.com>
2023-11-14 18:15:12 -0600

    bug

--- mood_magic/lib/main.dart
+++ mood_magic/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo', 
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mood Magic'),
     );
   }
 }
@@ -106,9 +106,9 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Push',
             ),
-            Text(
+            Text( 
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
