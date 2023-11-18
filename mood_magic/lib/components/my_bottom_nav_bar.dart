55950475a276492023cf1b2c0dd577595e3df18b
tlee85 <114027715+tlee85@users.noreply.github.com>
2023-11-18 00:23:19 -0600

    nav bar

--- mood_magic/lib/components/my_bottom_nav_bar.dart
+++ mood_magic/lib/components/my_bottom_nav_bar.dart
@@ -21,12 +21,12 @@ class MyBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
+      color: Colors.white,
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
       child: GNav(
-        color: Colors.white,
+        color: Colors.black,
         activeColor: Colors.white,
-        tabBackgroundColor: Colors.grey.shade800,
+        tabBackgroundColor: Color.fromRGBO(139, 76, 252, 50),
         padding: const EdgeInsets.all(16),
         gap: 8,
         onTabChange: (value) => onTabChange!(value),
@@ -36,16 +36,16 @@ class MyBottomNavBar extends StatelessWidget {
             text: 'Home',
           ),
           GButton(
-            icon: Icons.shopping_bag_rounded,
-            text: 'Shop',
+            icon: Icons.calendar_today,
+            text: 'Calender',
           ),
           GButton(
             icon: Icons.person,
             text: 'Profile',
           ),
           GButton(
-            icon: Icons.settings,
-            text: 'Settings',
+            icon: Icons.people,
+            text: 'Social',
           ),
         ],
       ),
